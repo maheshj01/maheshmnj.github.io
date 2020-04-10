6f1ec1ce9e4aa08b45f38082f34bc713f966da1a
mac006 <maheshmn121@gmail.com>
2020-04-10 17:06:04 +0530

    updated neumorphism tool

--- neumorphism/flutter_service_worker.js
+++ neumorphism/flutter_service_worker.js
@@ -1,17 +1,18 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"/assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
-"/assets/LICENSE": "1c29b85e9d84f6a9d6eee2368f0960f6",
-"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"/main.dart.js": "322377c220de2064f11699e102ed3496",
-"/index.html": "ee8cc1db5cdb57f4cd774aab9170f2cc",
-"/manifest.json": "7bf13c864c310ebf299ff634d2709b76",
-"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1"
+  "index.html": "ee8cc1db5cdb57f4cd774aab9170f2cc",
+"main.dart.js": "9d96105833718219c8ba1795769300ed",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"manifest.json": "7bf13c864c310ebf299ff634d2709b76",
+"assets/LICENSE": "2ee1ec44e15ff3b23babe2470dd157c8",
+"assets/AssetManifest.json": "0d266ffbe90dae02458487c9d33b7373",
+"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/packages/flutter_markdown/assets/logo.png": "67642a0b80f3d50277c44cde8f450e50",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
 };
 
 self.addEventListener('activate', function (event) {
