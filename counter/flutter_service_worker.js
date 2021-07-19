9317e09435d8b1b392b83079e0c4dedd0ad32d98
mahesh jamdade <maheshmn121@gmail.com>
2021-07-19 11:42:54 +0530

    update counter app

--- counter/flutter_service_worker.js
+++ counter/flutter_service_worker.js
@@ -4,15 +4,15 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "abb1b29d1c444059db4bf16f8a625c89",
-"index.html": "fd0c9448549fa312e7f0cda0b8e1a202",
-"/": "fd0c9448549fa312e7f0cda0b8e1a202",
-"main.dart.js": "9ddeaafc4fe28fb7c75153f3f2c4d8e7",
+"index.html": "86dd49affff7ed993e84a4e6241ef806",
+"/": "86dd49affff7ed993e84a4e6241ef806",
+"main.dart.js": "4f9e7d8c905c7d3ec02c5738d6ea3a68",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "83cd9613130820bb1ebae57c0c659e2b",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "52ca53fa69fabcb74bf0cd880307a342",
+"assets/NOTICES": "310f895257433cd4e5f76e76f7251d0d",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1"
