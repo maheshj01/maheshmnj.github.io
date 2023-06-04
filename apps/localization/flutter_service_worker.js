e35ccdb40ab70f7504342eb662f958ada7e9c3e4
mahesh jamdade <maheshmn121@gmail.com>
2023-06-04 00:52:09 -0400

    html renderer

--- apps/localization/flutter_service_worker.js
+++ apps/localization/flutter_service_worker.js
@@ -5,9 +5,9 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "d7ed79ecb75e8e3b09b837a84c7dc6db",
 "favicon.ico": "37c9bd95a996ed3301f8efcb19e161b6",
-"index.html": "57dbcc4156273b45fb288bef4b1fb0cc",
-"/": "57dbcc4156273b45fb288bef4b1fb0cc",
-"main.dart.js": "d1ebf4f5ec422fac60eab41dfc3df39d",
+"index.html": "1ac851b52398f5071192305bd1369bbe",
+"/": "1ac851b52398f5071192305bd1369bbe",
+"main.dart.js": "7898617f6acf43a3f6aa068cd9226c38",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
